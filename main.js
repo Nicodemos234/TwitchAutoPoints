b5d5b0024b7e5edbfa11b589bfe0742bc65a064d
Nicodemos234 <nicodemos54324@gmail.com>
2020-07-13 15:40:52 -0300

    Clicking

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-alert('teste');
+console.log('starting...');
+setTimeout(function () {
+    if (document.querySelector('.claimable-bonus__icon')) {
+        document.querySelector('.claimable-bonus__icon').click();
+        console.log('clicking...');
+    }
+    console.log('no exist');
+}, 1000);
