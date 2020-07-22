489e6e47031141aadaacc596e9fb7f59a20fdf3e
Nicodemos234 <nicodemos54324@gmail.com>
2020-07-22 12:11:58 -0300

    Clicking...

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 console.log('starting...');
-setTimeout(function () {
+setInterval(function () {
     if (document.querySelector('.claimable-bonus__icon')) {
         document.querySelector('.claimable-bonus__icon').click();
         console.log('clicking...');
     }
     console.log('no exist');
 }, 1000);
+// function recursive() {
+//     console.log("tentando");
+
+//     setInterval(recursive, 5000);
+// }
+
+// recursive();
